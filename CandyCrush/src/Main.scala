6dc21d169bbac315222a42e0c4888f8d8e44fa86
alej1000 <alejandro.raboso@edu.uah.es>
2023-04-07 09:44:21 +0200

    Funciones utiles para la tarea 4.3 y añadido generar matrices aleatorias

--- CandyCrush/src/Main.scala
+++ CandyCrush/src/Main.scala
@@ -1,3 +1,4 @@
+import scala.util.Random
 object Main { //Object, instancia unica que se utiliza en todo el programa
   def main(args: Array[String]): Unit = { //Funcion principal del programa -> Si se ejecuta en terminal: scala Main.scala sino no hace falta llamarla
     println("Hello world!")
@@ -6,7 +7,8 @@ object Main { //Object, instancia unica que se utiliza en todo el programa
     //Inicializar semilla randoms
 
     //Inicializar tablero con numeros aleatorios
-    crearMatriz(10, 10, 0)
+    crearMatriz(10, 10, 0) //Genera la matriz con 0s
+    generarMatriz(10, 10) //Genera la matriz con numeros aleatorios
 
     //Bucle principal del juego -> while(vida > 0) -> Se pueden bucles while? o tiene que ser recursivo?
     //Recursivo: funcion_principal(tablero,fila,columna,vidas)
@@ -70,9 +72,79 @@ object Main { //Object, instancia unica que se utiliza en todo el programa
     }
   }
 
+  //Esto de aquí es de la tarea 4.3 que puede servir para generar y coger los elementos de manera recursiva
+  def toma(n: Int, l: List[Int]): List[Int] = {
+    //Coge los n primeros elementos de la lista de manera recursiva
+    if (n == 0) Nil
+    else l.head :: toma(n - 1, l.tail)
+  }
+
+  def deja(n: Int, l: List[Int]): List[Int] = {
+    //Deja una lista sin los n primeros elementos de la lista de manera recursiva
+    if (n == 0) l
+    else deja(n - 1, l.tail)
+  }
+
+  def longitud[T](l: List[T]): Int = {
+    l match {
+      case Nil => 0 //Si la lista está vacía la longitud es 0
+      case _ :: tail => 1 + longitud(tail) //Si no está vacía la longitud es 1 + la longitud de la cola
+    }
+  }
+
+  //Genero la matriz aleatoriamente
+  def generarMatriz(filas: Int, columnas: Int): List[Int] = {
+    if (filas == 0) Nil
+    else {
+      concatenar(generarColumnas(columnas), generarMatriz(filas - 1, columnas))
+    }
+  }
+
+  def generarColumnas(n: Int): List[Int] = {
+    val rand = new Random()
+    if (n == 0) Nil
+    else rand.nextInt(100) :: generarColumnas(n - 1) //Genero un número aleatorio entre 0 y 99
+  }
+
+  def imprimirMatriz(matriz: List[Int]): Unit = {
+    matriz match {
+      case Nil => println()
+      case head :: tail => {
+        //print("|"+head+"|")
+        printf("| %3s |", head)
+        if (longitud(tail) % 8 == 0) println()
+        imprimirMatriz(tail)
+      }
+    }
+  }
+
+  def getElem(index: Int, matriz: List[Int]): Int = { //Prefiero crearla con fila:Int y columna:Int, antes que pasarle el indice directamente
+    matriz match {
+      case Nil => throw new Exception("El índice no es válido")
+      case head :: tail => {
+        if (index == 0) head
+        else getElem(index - 1, tail)
+      }
+    }
+  }
+
+  def getFila(fila: Int, matriz: List[Int]): List[Int] = {
+    toma(8, deja(fila * 8, matriz)) //Coge los 8 primeros elementos de la lista que queda de dejar todo menos los 8 primeros
+    //La matriz tiene 8 filas y 8 columnas
+    //toma(numColumnas,deja(filaElegida * numColumnas,matriz))
+  }
+
+  def getColumna(index: Int, matriz: List[Int]): List[Int] = {
+    if (index >= 0 && index < 8 && longitud(matriz) >= 8) { //Si el índice es válido -> 0<=index<numColumnas y longitud>=numColumnas
+      getElem(index, matriz) :: getColumna(index, deja(8, matriz))
+    } else {
+      Nil
+    }
+  }
+
   def eliminarPosicion(tablero:List[List[Int]], fila:Int, columna:Int):List[List[Int]] = {
     //Elimina (pone a 0) la posicion indicada
-    buscarJuntos(tablero, fila, columna,0) //Puedo hacer que devuelva el contador despues de haberlas eliminado?
+    buscarJuntos(tablero, fila, columna,0) //Puedo hacer que devuelva el contador despues de haberlas eliminado? -> Asi ya tengo el numero de fichas que he eliminado
     tablero
   }
 
